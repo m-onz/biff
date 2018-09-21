876326c3fe1a101a013b5f30729f383e264eb1f5
m-onz <stphnmnslw@gmail.com>
2018-09-21 23:34:14 +0100

    edit

--- example.js
+++ example.js
@@ -1,1 +1,14 @@
-console.log('> winning')
+
+console.log('self>', self)
+
+send(self, 'turnips')
+receive(self).then(function (x) {
+	console.log(x)
+	receive(self).then(function (x) {
+		console.log(x)
+		receive(self).then(function (x) {
+			console.log(x)
+			spawn('function a () { console.log(22); spawn("console.log(55)") } a();')
+		})
+	})
+})

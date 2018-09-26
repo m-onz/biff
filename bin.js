d0d8ae2bcb6c6a4726b6d29a81c0824afde7e6cc
m-onz <stphnmnslw@gmail.com>
2018-09-26 13:45:55 +0100

    add cli

--- bin.js
+++ bin.js
@@ -2,11 +2,10 @@
 
 var assert = require('assert')
 var path = require('path')
-var server = require('./server.js')()
-var biff = require('./client.js')()
-var vm   = require('vm')
-var fs   = require('fs')
-var net  = require('net')
+var biff = require('./biff.js')()
+var vm = require('vm')
+var fs = require('fs')
+var net = require('net')
 var http = require('http')
 
 // exposed node interfaces
@@ -25,16 +24,13 @@ global.kill = biff.kill
 global.self = '\n\n FAILURE! you are trying to run an actor in parent scope!' +
 '\n\n Try running npm start instead\n\n'
 
-server.listen(9999, function () {
-  console.log('server running')
-  try {
-    var args = process.argv.slice(2)
-    var file = args[0]
-    var _script = fs.readFileSync(path.normalize(file))
+try {
+  var args = process.argv.slice(2)
+  var file = args[0]
+  var _script = fs.readFileSync(path.normalize(file))
 
-    //  biff
-    var script = new vm.Script(_script, { filename: 'biff.vm' })
-    script.runInThisContext()
+  //  biff
+  var script = new vm.Script(_script, { filename: 'biff.vm' })
+  script.runInThisContext()
 
-  } catch (e) { throw e; }
-})
+} catch (e) { throw e; }
--- test.js
+++ test.js
@@ -1,39 +1,45 @@
 
-var assert = require('assert')
-var _biff = require('./biff.js')
-
-var biff = _biff ()
-
-biff.spawn(function () {
-  console.log(self, 11)
-})
-
-biff.spawn (function () {
-	console.log('inside a spawned function! ', self)
-	send (self, { a: Math.random() })
-	setTimeout(1000).then(function () {
-		receive(function (message) {
-			console.log(message)
-			receive(function (message) {
-				console.log(message)
-				exit()
-			})
-		})
-	})
-}, function (e, pid) {
-	if (e) return console.log(e)
-	var mailbox = biff.receive(pid)
-  var once = true
-	mailbox.on('message', function (message) {
-		console.log(message)
-    if (once) {
-      assert.ok(typeof message[0].a === 'number')
-      once = false
-    } else {
-      // assert mailbox gets flushed
-      console.log('tests passed')
-      assert.ok(message.length === 0)
-      process.exit(0)
-    }
-	})
+spawn(function () {
+  setTimeout(500).then(function () {
+    receive(function (message) {
+      console.log(message)
+    })
+  })
+  function fibonacci(num) {
+    if (num <= 1) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2)
+  }
+  send(self, { fib: 22, result: fibonacci (22) })
+}, function () {
+  setTimeout(function () {
+    spawn (function () {
+    	console.log('inside a spawned function! ', self)
+    	send (self, { a: Math.random() })
+    	setTimeout(1000).then(function () {
+    		receive(function (message) {
+    			console.log(message)
+    			receive(function (message) {
+    				console.log(message)
+    				exit()
+    			})
+    		})
+    	})
+    }, function (e, pid) {
+    	if (e) return console.log(e)
+    	var mailbox = receive(pid)
+      var once = true
+    	mailbox.on('message', function (message) {
+    		console.log(message)
+        if (once) {
+          assert.ok(typeof message[0].a === 'number')
+          once = false
+        } else {
+          // assert mailbox gets flushed
+          console.log('tests passed')
+          assert.ok(message.length === 0)
+          process.exit(0)
+        }
+    	})
+    })
+  }, 1000)
 })
